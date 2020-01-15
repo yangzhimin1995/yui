50dbded9aa8676bb0c7484ef2b74a1ce7543b096
yang <yangzhimin@dns.com>
2020-01-15 18:06:53 +0800

    anchorMenu

--- yui/yui.js
+++ yui/yui.js
@@ -282,27 +282,27 @@ function yui_handleModuleRoll(scrollTop, modulesDom, menuItemDom, checkedLineDom
         let menuDom = document.querySelector("div[yui-anchor-menu]>a[value=" + value + "]");
         if (module.offsetTop - scrollTop < top) {
             if (!menuDom.hasAttribute("checked")) {
-                yui_menuCheck(menuItemDom, menuDom)
+                yui_menuCheck(menuItemDom, menuDom);
                 //控制选中线
-                yui_menuCheckLine(checkedLineDom, menuItemDom);
+                yui_menuCheckLine(checkedLineDom, menuDom);
             }
         }
-        if (index === 0) {
-            if (menuDom.offsetTop - scrollTop > top) {
-                //清除所有选中，选中当前
-                yui_menuCheck(menuItemDom);
-                //控制选中线
-                yui_menuCheckLine(checkedLineDom);
-            }
-        }
-        if (index === modulesDom.length - 1) {
-            if (menuDom.offsetTop + menuDom.clientHeight - scrollTop < top) {
-                //清除所有选中，选中当前
-                yui_menuCheck(menuItemDom);
-                //控制选中线
-                yui_menuCheckLine(checkedLineDom);
-            }
-        }
+        // if (index === 0) {
+        //     if (menuDom.offsetTop - scrollTop > top) {
+        //         //清除所有选中，选中当前
+        //         yui_menuCheck(menuItemDom);
+        //         //控制选中线
+        //         yui_menuCheckLine(checkedLineDom);
+        //     }
+        // }
+        // if (index === modulesDom.length - 1) {
+        //     if (menuDom.offsetTop + menuDom.clientHeight - scrollTop < top) {
+        //         //清除所有选中，选中当前
+        //         yui_menuCheck(menuItemDom);
+        //         //控制选中线
+        //         yui_menuCheckLine(checkedLineDom);
+        //     }
+        // }
     })
 }
 
