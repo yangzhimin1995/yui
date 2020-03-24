9f8da8775cb84ac70a251f958505f0fdeac7dd23
yang <yangzhimin@dns.com>
2020-03-24 15:09:21 +0800

    alert

--- yui/js/index.js
+++ yui/js/index.js
@@ -21,6 +21,15 @@ function yui_init() {
 /** ================================= 全局方法 start =================================*/
 
 /**
+ * 横杠转驼峰
+ */
+function yui_getCamelCase(str) {
+    return str.replace(/-([a-z])/g, function (all, i) {
+        return i.toUpperCase();
+    })
+}
+
+/**
  * 批量增加属性
  */
 function yui_addAttributes(dom, attributes = {}) {
@@ -35,7 +44,8 @@ function yui_addAttributes(dom, attributes = {}) {
 function yui_getAttributes(dom, attributes = []) {
     let result = {};
     attributes.forEach(item => {
-        result[item] = dom.getAttribute(item);
+        const key = yui_getCamelCase(item);
+        result[key] = dom.getAttribute(item);
     });
     return result
 }
@@ -66,66 +76,29 @@ function yui_addStyles(dom, styles = {}) {
 /** ================================= Alert start =================================*/
 
 function yuiAlert_init() {
-    let dom = document.querySelectorAll('div[yui-alert]');
-    // dom.forEach(alertDom => {
-    //     let attributes = yui_getAttributes(alertDom, [
-    //         'type',
-    //         'closable',
-    //         'close-text',
-    //         'show-icon',
-    //         'before-close'
-    //     ]);
-    //     alertDom.innerHTML = '<div><div yui-alert-content>' + alertDom.innerHTML + '</div></div>';
-    //     let closeDom = yuiAlert_handleCloseIcon(alertDom, attributes['closable'], attributes['close-text']);
-    //     if (closeDom) {
-    //         yuiAlert_handleClose(alertDom, closeDom, attributes['before-close']);
-    //     }
-    //     yuiAlert_handleTypeIcon(alertDom, attributes['show-icon'], attributes['type'])
-    // })
-}
-
-function yuiAlert_handleTypeIcon(dom, showIcon, type) {
-    if (showIcon !== null && showIcon !== 'false') {
-        let iconDom = document.createElement('i');
-        if (type !== 'success' && type !== 'warning' && type !== 'danger') {
-            type = 'info'
+    let dom = document.querySelectorAll('[yui-alert]');
+    dom.forEach(alertDom => {
+        const closeDom = alertDom.querySelector('[close-text]');
+        if (closeDom) {
+            const domRemove = dom => {
+                yui_addStyles(dom, {opacity: '0'});
+                setTimeout(() => {
+                    dom.remove()
+                }, 200)
+            };
+            const {beforeClose} = yui_getAttributes(alertDom, ['before-close']);
+            closeDom.addEventListener("click", function () {
+                debugger
+                if (beforeClose) {
+                    let result = eval(`${beforeClose}(alertDom)`);
+                    if (result === false) {
+                        return
+                    }
+                }
+                domRemove(alertDom)
+            });
         }
-        yui_addClasses(iconDom, ['iconfont', `yui-icon-${type}`]);
-        yui_addAttributes(iconDom, {'type-icon': ''});
-        dom.firstChild.prepend(iconDom);
-    }
-}
-
-function yuiAlert_handleCloseIcon(dom, closable, text) {
-    if (closable !== 'false') {
-        let closeDom = document.createElement('i');
-        yui_addAttributes(closeDom, {'close-icon': ''});
-        if (text) {
-            closeDom.innerText = text;
-        } else {
-            yui_addClasses(closeDom, ['iconfont', 'yui-icon-closed']);
-        }
-        dom.appendChild(closeDom);
-        return closeDom;
-    }
-}
-
-function yuiAlert_handleClose(dom, closeDom, beforeClose) {
-    const domRemove = dom => {
-        yui_addStyles(dom, {opacity: '0'});
-        setTimeout(() => {
-            dom.remove()
-        }, 200)
-    };
-    closeDom.addEventListener("click", function () {
-        if (beforeClose) {
-            let result = eval(`${beforeClose}(dom)`);
-            if (result === false) {
-                return
-            }
-        }
-        domRemove(dom)
-    });
+    })
 }
 
 /** ================================= Alert end =================================*/
